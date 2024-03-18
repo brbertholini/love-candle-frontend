d926571e2bcf8842fd90afc410c48f58e657109e
Bruno Bertholini <brbertholini@gmail.com>
2024-03-18 14:19:43 -0300

    FEAT: Starting Cart page

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    width: 100vw;
+`
+
+export const Content = styled.div`
+    width: 65vw;
+    padding: 0 15vw;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+`
+
+export const CartInfo = styled.div`
+
+`
