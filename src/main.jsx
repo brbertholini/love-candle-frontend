fb71f9d45559f8a059c0438d0614c32ea0006833
Bruno Bertholini <brbertholini@gmail.com>
2024-03-18 14:23:31 -0300

    FEAT: Site Title and Icon

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyle from './styles/global';
 
-import { Home } from './pages/Home';
-import { Contact } from './pages/Contact';
+import { Cart } from './pages/Cart';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
-    <Home/>
+    <Cart/>
   </React.StrictMode>,
 )
