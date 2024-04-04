03e68fe5ddba228c1ed17880ec29ce7b2e7e8862
Bruno Bertholini <brbertholini@gmail.com>
2024-04-04 20:21:01 -0300

    FEAT: Buttons created on AdminPanel

--- src/pages/AdminPanel/index.jsx
+++ src/pages/AdminPanel/index.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react"
 import { Sidebar } from "../../components/Sidebar"
 import { api } from "../../services/api.js";
-import { Clients, Container, Content, Materials, Orders, Products, StyledTd, StyledTh, Table } from "./styles.js"
+import { LuListFilter } from "react-icons/lu";
+import { GoPlus } from "react-icons/go";
+
+import { ButtonArea, Clients, Container, Content, Filter, FilterArea, Input, Materials, NewProduct, Orders, Products, SearchIcon, StyledTd, StyledTh, Table } from "./styles.js"
 
 export function AdminPanel() {
     const [activeIcon, setActiveIcon] = useState('');
@@ -26,15 +29,24 @@ export function AdminPanel() {
                 {activeIcon === 'orders' && (
                     <Orders>
                         <h1>PEDIDOS</h1>
-                        <p>Todos os pedios da sua loja estarão presentes nessa lista.
+                        <p className="description">Todos os pedios da sua loja estarão presentes nessa lista.
                             Você pode editar o status ou excluir algum pedido.</p>
+                        <p className="quantity" style={{ color: '#6D6D6D' }}>{ } pedidos encontrados</p>
                     </Orders>
                 )}
                 {activeIcon === 'products' && (
                     <Products>
                         <h1>PRODUTOS</h1>
-                        <p>Todos os produtos disponíveis na sua loja estarão presentes nessa lista.
+                        <p className="description">Todos os produtos disponíveis na sua loja estarão presentes nessa lista.
                             Você pode editá-los, excluí-los ou criar novos produtos.</p>
+                        <ButtonArea>
+                            <FilterArea>
+                            <SearchIcon /><Input placeholder="Buscar produto" />
+                            <Filter><LuListFilter />Filtros</Filter>
+                            </FilterArea>
+                            <NewProduct><GoPlus style={{ marginRight: '10px'}} />Adicionar novo produto</NewProduct>
+                        </ButtonArea>
+                        <p className="quantity" style={{ color: '#6D6D6D' }}>{products.length} produtos encontrados</p>
                         <Table>
                             <thead>
                                 <tr>
@@ -45,6 +57,7 @@ export function AdminPanel() {
                                     <StyledTh style={{ textAlign: 'left' }}>QUANTIDADE</StyledTh>
                                     <StyledTh style={{ textAlign: 'left' }}>VALOR</StyledTh>
                                     <StyledTh style={{ textAlign: 'left' }}>DETALHES</StyledTh>
+                                    <StyledTh style={{ textAlign: 'left' }}>EDITAR</StyledTh>
                                 </tr>
                             </thead>
                             <tbody>
@@ -56,7 +69,8 @@ export function AdminPanel() {
                                         <StyledTd>{product.weight}</StyledTd>
                                         <StyledTd>{product.amount}</StyledTd>
                                         <StyledTd>{product.price}</StyledTd>
-                                        <StyledTd><a>Clique para ver</a></StyledTd>
+                                        <StyledTd>Clique para ver</StyledTd>
+                                        <StyledTd>Editar</StyledTd>
                                     </tr>
                                 ))}
                             </tbody>
@@ -66,14 +80,16 @@ export function AdminPanel() {
                 {activeIcon === 'materials' && (
                     <Materials>
                         <h1>RECURSOS</h1>
-                        <p>Todos os materiais de produção disponíveis no seu estoque
+                        <p className="description">Todos os materiais de produção disponíveis no seu estoque
                             estarão presentes nessa lista. Você pode editá-los, excluí-los ou criar novos.</p>
+                        <p className="quantity" style={{ color: '#6D6D6D' }}>{ } recursos encontrados</p>
                     </Materials>
                 )}
                 {activeIcon === 'clients' && (
                     <Clients>
                         <h1>CLIENTES</h1>
-                        <p>Todos os usuários registrados na sua loja estarão presentes nessa lista.</p>
+                        <p className="description">Todos os usuários registrados na sua loja estarão presentes nessa lista.</p>
+                        <p className="quantity" style={{ color: '#6D6D6D' }}>{ } clientes encontrados</p>
                     </Clients>
                 )}
             </Content>
--- src/pages/AdminPanel/styles.js
+++ src/pages/AdminPanel/styles.js
@@ -1,4 +1,16 @@
 import styled from "styled-components";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+
+export const SearchIcon = styled(HiMagnifyingGlass)`
+    font-size: 22px;
+    margin-right: 6px;
+`
+
+export const FilterArea = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
 
 export const Container = styled.div`
     width: 100%;
@@ -14,6 +26,74 @@ export const Content = styled.div`
     flex-grow: 1;
 `
 
+export const ButtonArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-bottom: 24px;
+`
+
+export const Input = styled.input`
+    font-size: 16px;
+    height: 2.2rem;
+    width: fit-content;
+    padding: 10px;
+    border: 0.5px solid black;
+    background-color: #FFF4EE;
+    -webkit-box-shadow: -4px 0px 15px 0px rgba(0,0,0,0.30);
+    -moz-box-shadow: -4px 0px 15px 0px rgba(0,0,0,0.30);
+    box-shadow: -4px 0px 15px 0px rgba(0,0,0,0.30);
+`
+
+export const Filter = styled.button`
+    margin-left: 25px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    gap: 6px;
+    height: 2.2rem;
+    width: 6.5rem;
+    padding: 10px;
+    background-color: black;
+    border: none;
+    
+    transition: background-color 600ms, color 600ms, border 600ms, transform 600ms;
+
+    &:hover {
+        background-color: transparent;
+        color: black;
+        border: 1px solid black;
+        transform: scale(1.03);
+        transition: all 600ms;
+    }
+`
+
+export const NewProduct = styled.button`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    gap: 6px;
+    height: 2.2rem;
+    width: fit-content;
+    padding-left: 20px;
+    padding-right: 20px;
+    background-color: #17A500;
+    border: none;
+
+    transition: background-color 600ms, color 600ms, border 600ms, transform 600ms;
+
+    &:hover {
+    transform: scale(1.03);
+    transition: all 600ms;
+    background-color: #17A500;
+    }
+`
+
 export const Orders = styled.div`
     > h1 {
         font-family: 'DM Serif Display', serif;
@@ -21,9 +101,13 @@ export const Orders = styled.div`
         margin-bottom: 8px;
     }
 
-    > p {
+    > .description {
         width: 53%;
-        margin-bottom: 50px;
+        padding-bottom: 50px;
+    }
+
+    > .quantity {
+        padding-bottom: 8px;
     }
 `
 
@@ -34,9 +118,13 @@ export const Products = styled.div`
         margin-bottom: 8px;
     }
 
-    > p {
+    > .description {
         width: 53%;
-        margin-bottom: 50px;
+        padding-bottom: 50px;
+    }
+
+    > .quantity {
+        padding-bottom: 8px;
     }
 `
 
@@ -47,9 +135,13 @@ export const Materials = styled.div`
         margin-bottom: 8px;
     }
 
-    > p {
+    > .description {
         width: 53%;
-        margin-bottom: 50px;
+        padding-bottom: 50px;
+    }
+
+    > .quantity {
+        padding-bottom: 8px;
     }
 `
 
@@ -60,9 +152,13 @@ export const Clients = styled.div`
         margin-bottom: 8px;
     }
 
-    > p {
+    > .description {
         width: 53%;
-        margin-bottom: 50px;
+        padding-bottom: 50px;
+    }
+
+    > .quantity {
+        padding-bottom: 8px;
     }
 `
 
@@ -72,7 +168,7 @@ export const Table = styled.table`
 `
 
 export const StyledTh = styled.th`
-    padding-bottom: 3px;
+    padding-bottom: 0.4rem;
     font-weight: 900;
 `
 
